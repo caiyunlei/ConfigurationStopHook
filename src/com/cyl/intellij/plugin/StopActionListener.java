398986267487de4d124483e1df2185e5093210a5
caiyunlei <cyl1014053448@gmail.com>
2020-01-13 05:36:18 +0800

    refactor: delete unused class

--- src/com/cyl/intellij/plugin/StopActionListener.java
+++ src/com/cyl/intellij/plugin/StopActionListener.java
@@ -1,14 +1,23 @@
 package com.cyl.intellij.plugin;
 
-import com.cyl.intellij.plugin.actions.BeforeTerminalTasksAction;
+import com.cyl.intellij.plugin.settings.TasksSettings;
 import com.intellij.application.Topics;
+import com.intellij.execution.Executor;
+import com.intellij.execution.RunManager;
+import com.intellij.execution.RunnerAndConfigurationSettings;
+import com.intellij.execution.configurations.RunConfiguration;
+import com.intellij.execution.executors.DefaultRunExecutor;
+import com.intellij.execution.runners.ExecutionUtil;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.ex.AnActionListener;
+import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class StopActionListener implements AnActionListener, Disposable {
     private static final String STOP_ACTION_ID = "Stop";
@@ -20,13 +29,26 @@ public class StopActionListener implements AnActionListener, Disposable {
     @Override
     public void afterActionPerformed(@NotNull AnAction action, @NotNull DataContext dataContext, @NotNull AnActionEvent event) {
         if (isStopAction(action)) {
-            BeforeTerminalTasksAction.runTasks(event.getProject());
+            runTasks(event.getProject());
         }
     }
 
     private boolean isStopAction(AnAction action) {
         String myIdeaActionID = ActionManager.getInstance().getId(action);
         return STOP_ACTION_ID.equals(myIdeaActionID);
+    }
+
+    private static void runTasks(Project p) {
+        RunnerAndConfigurationSettings stopSettings = RunManager.getInstance(p).getSelectedConfiguration();
+        RunConfiguration runConfiguration = stopSettings.getConfiguration();
+        Executor executor = DefaultRunExecutor.getRunExecutorInstance();
+
+        List<RunnerAndConfigurationSettings> beforeRunConfigurations = TasksSettings.getInstance().getBeforeTerminalTasks(runConfiguration);
+        for (RunnerAndConfigurationSettings runnerAndConfigurationSettings : beforeRunConfigurations) {
+            if (runnerAndConfigurationSettings != null) {
+                ExecutionUtil.runConfiguration(runnerAndConfigurationSettings, executor);
+            }
+        }
     }
 
     @Override
