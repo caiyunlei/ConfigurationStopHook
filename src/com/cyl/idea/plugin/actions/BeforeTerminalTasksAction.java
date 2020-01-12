2b8837f9129252a252a9a92e24bc8693fd1553da
caiyunlei <cyl1014053448@gmail.com>
2020-01-12 22:04:47 +0800

    feature: persisten config info, save to config file and load from config file.

--- src/com/cyl/idea/plugin/actions/BeforeTerminalTasksAction.java
+++ src/com/cyl/idea/plugin/actions/BeforeTerminalTasksAction.java
@@ -7,7 +7,6 @@ import com.intellij.execution.RunnerAndConfigurationSettings;
 import com.intellij.execution.actions.StopAction;
 import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.execution.executors.DefaultRunExecutor;
-import com.intellij.execution.impl.RunConfigurationBeforeRunProvider;
 import com.intellij.execution.runners.ExecutionUtil;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
@@ -24,18 +23,18 @@ public class BeforeTerminalTasksAction extends StopAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
         Project myProject = e.getProject();
-        RunnerAndConfigurationSettings settings =
-            RunManager.getInstance(myProject).getSelectedConfiguration();
-        RunConfiguration runConfiguration = settings.getConfiguration();
-        Executor executor = DefaultRunExecutor.getRunExecutorInstance();
-        List<RunConfigurationBeforeRunProvider.RunConfigurableBeforeRunTask> beforeRunTasks =
-            TasksSettings.getInstance().getBeforeTerminalTasks(runConfiguration);
 
-        for (RunConfigurationBeforeRunProvider.RunConfigurableBeforeRunTask beforeRunTask :
-            beforeRunTasks) {
-            if (beforeRunTask != null) {
-                RunnerAndConfigurationSettings tasksSettings = beforeRunTask.getSettings();
-                ExecutionUtil.runConfiguration(tasksSettings, executor);
+        RunnerAndConfigurationSettings stopSettings =
+            RunManager.getInstance(myProject).getSelectedConfiguration();
+        RunConfiguration runConfiguration = stopSettings.getConfiguration();
+        Executor executor = DefaultRunExecutor.getRunExecutorInstance();
+
+        List<RunnerAndConfigurationSettings> beforeRunConfigurations =
+            TasksSettings.getInstance().getBeforeTerminalTasks(runConfiguration);
+        for (RunnerAndConfigurationSettings runnerAndConfigurationSettings :
+            beforeRunConfigurations) {
+            if (runnerAndConfigurationSettings != null) {
+                ExecutionUtil.runConfiguration(runnerAndConfigurationSettings, executor);
             }
         }
 
